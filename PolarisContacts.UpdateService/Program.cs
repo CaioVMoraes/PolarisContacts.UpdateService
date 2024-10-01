9f8a5eb6631fd165daa241db1846f0437107af05
Caio Vinicius <caio.moraes@fiap.com.br>
2024-10-01 20:21:18 -0300

    prometheus

--- PolarisContacts.UpdateService/Program.cs
+++ PolarisContacts.UpdateService/Program.cs
@@ -1,4 +1,6 @@
 using PolarisContacts.UpdateService.CrossCutting.DependencyInjection;
+using Prometheus;
+using System.Diagnostics;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -41,6 +43,42 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+// Adiciona o middleware do Prometheus para métricas
+app.UseHttpMetrics();
+
+// Métricas personalizadas
+var requestCounter = Metrics.CreateCounter("myapp_http_requests_total", "Total number of HTTP requests received");
+var responseLatency = Metrics.CreateHistogram("myapp_http_response_latency_seconds", "HTTP response latency in seconds");
+
+// Métricas de uso de CPU e memória
+var cpuUsageGauge = Metrics.CreateGauge("myapp_cpu_usage_total", "Total CPU usage of the application");
+var memoryUsageGauge = Metrics.CreateGauge("myapp_memory_usage_bytes", "Total memory usage of the application in bytes");
+
+// Atualizar métricas de CPU e memória a cada requisição
+var process = Process.GetCurrentProcess();
+
+app.Use(async (context, next) =>
+{
+    // Incrementar o contador de requisições
+    requestCounter.Inc();
+
+    // Iniciar a medição de latência
+    var stopwatch = Stopwatch.StartNew();
+
+    // Executar a próxima parte do pipeline
+    await next.Invoke();
+
+    // Parar a medição de latência
+    stopwatch.Stop();
+    responseLatency.Observe(stopwatch.Elapsed.TotalSeconds);
+
+    // Atualizar métricas de uso de CPU e memória
+    cpuUsageGauge.Set(process.TotalProcessorTime.TotalMilliseconds / Environment.ProcessorCount);
+    memoryUsageGauge.Set(process.WorkingSet64);
+});
+
+app.MapMetrics(); // Rota para as métricas do Prometheus
 
 // Mapeia os controladores
 app.MapControllers();
